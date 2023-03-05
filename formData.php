cef8ee7bd8c4a4705ab813f32c99a06732c97001
armando9310262022 <armando.rojas17ce@cenidet.edu.mx>
2023-03-05 17:10:17 -0600

    nuevos cambios

---
+++ formData.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>formData</title>
+</head>
+<body>
+<?php
+
+	$user = $_POST['user'];
+	$passwd = $_POST['passwd'];
+		
+		echo "<b>Usuario:</b> ",$user, "<br>";
+		echo "<b>Contraseña:</b> ",$passwd;
+		
+?>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+</head>
+<body>
+<form method="post" action="formData.php">
+
+<h1>Iniciar Sesión:</h1>
+<table>
+	<tr>
+		<th>Usuario:</th>
+		<th><input type="text" name="user"></th>
+	</tr>
+	<tbody>
+		<td>Contraseña:</td>
+		<td><input type="password" name="passwd"></td>
+		<td><input type="submit" name="is" value="Iniciar Sesión"></td>
+	<tr>
+		<td></td>
+		<td>Recordar Contraseña<input type="radio" name=""></td>
+		<td><input type="button" name="rc" value="Recuperar Contraseña"></td>
+	</tr>	
+	</tbody>
+</table>
+
+</form>
+</body>
+</html>
